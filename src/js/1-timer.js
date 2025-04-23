20d71abce596b47f72d65ae02314e7a8e4a0ba09
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-04-23 13:53:31 +0100

    Check validity of date

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,6 +1,14 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let userSelectedDate;
+
+startBtn.disabled = true;
+stopBtn.disabled = true;
+
 const options = {
     enableTime: true,
     time_24hr: true,
@@ -9,7 +17,15 @@ const options = {
     minuteIncrement: 1,
     onClose(selectedDates) {
         console.log(selectedDates[0]);
+        userSelectedDate = selectedDates[0];
+        if (userSelectedDate < new Date()) {
+            window.alert('Please choose a date in the future');
+            startBtn.disabled = true;
+            stopBtn.disabled = true;
+        } else {
+            startBtn.disabled = false;
+            // stopBtn.disabled = false;
+        }
     },
 };
-
 flatpickr('#datetime-picker', options);
