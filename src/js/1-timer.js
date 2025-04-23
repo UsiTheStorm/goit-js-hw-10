54797eeb5a6b0d6b256d5f5f63b67972045f18d4
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-04-23 15:56:35 +0100

    Add pad function

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -4,11 +4,48 @@ import 'flatpickr/dist/flatpickr.min.css';
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+
 let userSelectedDate;
 
 startBtn.disabled = true;
 stopBtn.disabled = true;
 
+// Function to convert milliseconds to days, hours, minutes and seconds
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+}
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000));
+// console.log(convertMs(userSelectedDate - Date.now())); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+// Function to add leading zero
+function pad(value) {
+    return value.toString().padStart(2, '0');
+}
+
+// function startCountdoun()
+
+// Timer initialization
 const options = {
     enableTime: true,
     time_24hr: true,
@@ -29,26 +66,3 @@ const options = {
     },
 };
 flatpickr('#datetime-picker', options);
-
-function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-    return { days, hours, minutes, seconds };
-}
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
