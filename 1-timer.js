b12c6a13b0ed7ac0487920463b24a557b6450b85
UsiTheStorm <50480382+UsiTheStorm@users.noreply.github.com>
2025-04-23 11:59:12 +0000

    Deploying to gh-pages from @ UsiTheStorm/goit-js-hw-10@98a6418100dec4d3b68a6510081ea6674980067d 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/styles-CGQZsQCt.js";import{f as t}from"./assets/vendor-CCO2o_no.js";const o={enableTime:!0,time_24hr:!0,defaultDate:new Date,minDate:"today",minuteIncrement:1,onClose(e){console.log(e[0])}};t("#datetime-picker",o);
+//# sourceMappingURL=1-timer.js.map
