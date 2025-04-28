ed02199550b1fa2e751e69aceaa85f26827af478
UsiTheStorm <50480382+UsiTheStorm@users.noreply.github.com>
2025-04-28 15:23:37 +0000

    Deploying to gh-pages from @ UsiTheStorm/goit-js-hw-10@6ddf590c1377bb035ba663c4702b9376723cadf1 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,2 +1,2 @@
-import"./assets/styles-DstMkDFQ.js";import"./assets/vendor-B1CLq4HH.js";document.querySelector("#delay");document.querySelectorAll('button[name="state"]');const t=document.querySelector(".form");t.addEventListener("submit",o=>{o.preventDefault();const e=new FormData(t),a=e.get("delay"),l=e.get("state");console.log("Delay:",a),console.log("State:",l)});
+import"./assets/styles-DstMkDFQ.js";import"./assets/vendor-B1CLq4HH.js";const t=document.querySelector(".form");t.addEventListener("submit",o=>{o.preventDefault();const e=new FormData(t),a=e.get("delay"),l=e.get("state");console.log("Delay:",a),console.log("State:",l)});
 //# sourceMappingURL=2-snackbar.js.map
