dd6fd8d58e49aa00fa03f073d9d1fc80c8b3a4f4
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-04-28 17:43:14 +0100

    Add function to get promise

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,15 +1,26 @@
+/* eslint-disable prefer-promise-reject-errors */
 import { showSuccessToast, showErrorToast } from './utilitis/toasts';
 
 const form = document.querySelector('.form');
+
+// Function to get promise result
+function getPromise(delay, state) {
+    return new Promise((res, rej) => {
+        if (state === 'fullfilled') {
+            res(`Fulfilled promise in ${delay}ms`);
+        } else {
+            rej(`Rejected promise in ${delay}ms`);
+        }
+    });
+}
 
 form.addEventListener('submit', (evt) => {
     evt.preventDefault();
 
     const formData = new FormData(form);
-    const delayValue = formData.get('delay');
+    const delayValue = Number(formData.get('delay'));
     const stateValue = formData.get('state');
 
     console.log('Delay:', delayValue);
     console.log('State:', stateValue);
 });
-// const promise = ()
